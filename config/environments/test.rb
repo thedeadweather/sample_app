f21203f06f784dd619c160e98f6fe2b713c41fd6
thedeadweather <kalininmalinin@gmail.com>
2019-05-27 00:21:43 +0300

    accomplish user sign up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  #Speed up test by lowering bcrypt's cost funtion!
+  ActiveModel::SecurePassword.min_cost = true
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name   "Micael"
+    email  "alex@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
