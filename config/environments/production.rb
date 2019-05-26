176b9c157964557225de6c1ff5dd1bfe004fb928
thedeadweather <kalininmalinin@gmail.com>
2019-05-27 00:31:13 +0300

    add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+
+  # force all access to thr app over SSL, use Strict-Transport-Sequrity,
+  #and use secure cookies.
+  config.force_ssl = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
